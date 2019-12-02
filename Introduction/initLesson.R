c0dadb0671d870fa6f4141fcb69149ef1b1313ed
Isabella Bingaman <ib6708a@student.american.edu>
2019-12-01 23:40:33 -0500

    Add PolityIV

--- Introduction/initLesson.R
+++ Introduction/initLesson.R
@@ -2,3 +2,15 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
+
+# function for loading data 
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+# load PolityIV dataset 
+
+PolityIV <- read.csv(file.path(.get_course_path(), "PoliSwirl", "Introduction", "p4v2017d.csv"))
