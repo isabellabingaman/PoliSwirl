39584f40267b5eee305232f81d6f3995c97713dc
Isabella Bingaman <ib6708a@student.american.edu>
2019-12-03 06:55:49 -0500

    more

---
+++ Modeling/initLesson.R
@@ -0,0 +1,13 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+# function for loading data 
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
