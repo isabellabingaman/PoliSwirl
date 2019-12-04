0abb3b06bffa0608d3c72c9c2c147ce95636cfd6
Isabella Bingaman <ib6708a@student.american.edu>
2019-12-04 05:21:53 -0500

    fixing graphs again

--- ModelingMaps/map5.R
+++ ModelingMaps/map5.R
@@ -4,6 +4,8 @@ election <- socviz::election
 
 us_states <- map_data("state")
 
+state$region <- state2abbr(state$region)
+
 us_states_elec <- left_join(us_states, election)
 
 electionmap <- ggplot(data = us_states_elec, aes(x = long, y = lat, group = group, fill = party))
